9613d30b1d203ad65083d1effb9a6aa5f7874009
kirillkrohmal <krohmal_kirill@mail.ru>
2020-11-09 13:31:08 +0300

    first commit

--- src/main/resources/languages.sql
+++ src/main/resources/languages.sql
@@ -6,3 +6,10 @@ CREATE TABLE languages
     rating integer NOT NULL,
     CONSTRAINT languages_pkey PRIMARY KEY (id)
 );
+
+
+INSERT INTO languages VALUES (1, 'Java', 'Java language', 1);
+INSERT INTO languages VALUES (2, 'JavaScript', 'JavaScript language', 2);
+INSERT INTO languages VALUES (3, 'C#', 'C# language', 3);
+INSERT INTO languages VALUES (4, 'C++', 'C++ language', 4);
+INSERT INTO languages VALUES (5, 'Python', 'Python language', 5);
